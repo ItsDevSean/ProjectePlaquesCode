73b50b881922f285fef21a28be0937d88528b220
GarzonMiguelA <miguel.garzon.7e7@itb.cat>
2025-01-16 16:41:29 +0100

    commit

--- Miguel/backend/conn.php
+++ Miguel/backend/conn.php
@@ -1,11 +1,13 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$contraseña = "";
-$base_de_datos = "mondongo"; 
-    $conn = new mysqli($servidor, $usuario, $contraseña, $base_de_datos);
-    
-    if ($conn->connect_error) {
-        die("Conexion fallida". $conn->connect_error);
-    }
+$servidor = "sql7.freemysqlhosting.net";
+$usuario = "sql7757835";
+$contraseña = "rYr5ukw4nC";
+$base_de_datos = "sql7757835"; 
+$conn = new mysqli($servidor, $usuario, $contraseña, $base_de_datos);
+
+if ($conn->connect_error) {
+    die("Conexion fallida". $conn->connect_error);
+}else{
+    echo "Conexion OK";
+}
 ?>
