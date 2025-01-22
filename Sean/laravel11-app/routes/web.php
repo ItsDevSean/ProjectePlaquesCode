8f582fda747e43f90dd9d001b5704e3d3dfaea0c
SeanITB <sean.costelloe.7e5@itb.cat>
2025-01-22 09:55:59 +0100

    feat: learning the rutes

--- Sean/laravel11-app/routes/web.php
+++ Sean/laravel11-app/routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/myamoreterno', function () {
+
+    $age = 20;
+    $data = ['name' => 'Seán', 'age' => $age];
+
+    return view('crud.index', $data);
+})->name('folder');
+
+
