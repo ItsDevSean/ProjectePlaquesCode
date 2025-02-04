e77badd039633f5a9ebb9af1132584f35e771e02
Teo Castellví <146827242+TeoCastellvi@users.noreply.github.com>
2025-02-04 10:39:21 +0100

    Delete Sean directory

--- Sean/laravel11-app/app/php/test_pdo.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=test_laravel11', 'laravel_user', 'laravel11');
-    echo "PDO MySQL connection successful!";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-?>
