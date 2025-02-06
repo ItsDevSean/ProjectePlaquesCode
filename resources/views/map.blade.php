6fda0c35e5b09ae1099b6783ba4657b25b244f4d
TeoCastellvi <teo.castellvi.7e7@itb.cat>
2025-02-06 10:37:29 +0100

    Implementació del mapa i diverses funcionalitats

---
+++ resources/views/map.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscador de Dirección</title>
+    <link rel="stylesheet" href="build/css/styles.css">
+    <style>
+        #map {
+            height: 500px;
+            width: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Buscador de Dirección') }}
+            </h2>
+        </x-slot>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <div id="mapHeader">
+                            <!-- Formulario de búsqueda -->
+                            <div>
+                                <input type="text" id="address" placeholder="Escribe tu ciudad, calle y número">
+                                <button id="buttonBuscar" onclick="geocodeAddress()">Buscar</button>
+                            </div>
+
+                            <!-- Botones para cambiar el tipo de mapa -->
+                            <div class="map-buttons">
+                                <button onclick="changeMapType('roadmap')">Roadmap</button>
+                                <button onclick="changeMapType('satellite')">Satelite</button>
+                            </div>
+                        </div>
+
+                        <!-- Mapa donde se mostrará la localización -->
+                        <div id="map"></div>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Cargar el script de Google Maps -->
+        <script src="build/js/marcadors.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDu3ReEUVEQANj_h1EAtfe4-zyarcb3X04&libraries=places&callback=initMap"></script>
+    </x-app-layout>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/mapa', function () {
+    return view('map');
+})->name('map');
