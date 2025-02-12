e311ba0c7a1b4182c74e31ab330e513f0083f898
marcSantolayaSanchez <marc.santolaya.7e7@itb.cat>
2025-02-12 10:49:37 +0100

    navegacion a Opencv

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,7 +21,13 @@
                     <x-nav-link :href="route('map')" :active="request()->routeIs('map')">
                         {{ __('Mapa') }}
                     </x-nav-link>
-                </div>                
+                </div>      
+                
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('open')" :active="request()->routeIs('open')">
+                        {{ __('OpenCV') }}
+                    </x-nav-link>
+                </div>   
                 
             </div>
 
