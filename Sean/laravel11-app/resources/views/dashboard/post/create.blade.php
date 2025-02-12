63da4274bc91eaeb5af9a86d29ee3da094bbbc42
SeanITB <sean.costelloe.7e5@itb.cat>
2025-02-12 17:27:04 +0100

    feat: addig files

--- Sean/laravel11-app/resources/views/dashboard/post/create.blade.php
+++ Sean/laravel11-app/resources/views/dashboard/post/create.blade.php
@@ -1,10 +1,34 @@
 @extends('dashboard.master')
 
 @section('contect')
-    <form action="" method="POST" >
+    <form action="{{ route('post.store') }}" method="POST" >
 
+        @csrf
+
+        <label for="">Title</label>
         <input type="text" name="title">
 
+        <label for="">Slug</label>
+        <input type="text" name="title">
+
+        <label for="">Content</label>
+        <textarea name="content"></textarea>
+
+        <label for="">Categories</label>
+        <select name="categories_id">
+            @foreach ($categories as $title => $id)
+                <option value="{{ $id }}">{{$title}}</option>
+            @endforeach
+        </select>
+
+        <label for="">Posted</label>
+        <select name="posted">
+            <option value="Not">Not</option>
+            <option value="yes">Yes</option>
+        </select>
+
+        <label for="">Description</label>
+        <textarea name="description"></textarea>
         <button type="submit">Send</button>
     </form>
 
