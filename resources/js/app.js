a0d67c5dbf6b5436041e3be955f20f4efda3484e
marcSantolayaSanchez <marc.santolaya.7e7@itb.cat>
2025-02-17 11:23:31 +0100

    feat - Implementación de Vue. He creado los archivos necesarios para que vue funcione, solo queda gestionar la bd y subir la información de esta a la API web para luego con axios coger la información y mostrarlas en las views.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,18 @@
-import './bootstrap';
+import { createApp } from "vue";
+import App from "../views/vue/App.vue"; // Ruta correcta
+import router from "./router";
+import Oruga from "@oruga-ui/oruga-next";
+import "@oruga-ui/theme-oruga/dist/oruga.css"; 
+import axios from "axios";
+import "@mdi/font/css/materialdesignicons.min.css";
 import Alpine from 'alpinejs';
-import { createApp } from 'vue';
-import App from './components/App.vue';
-const app = createApp(App);
-app.mount('#app');
-window.Alpine = Alpine;
 
+const app = createApp(App);
+
+app.use(Oruga);
+app.use(router);
+app.config.globalProperties.$axios = axios;
+window.axios = axios;
+window.Alpine = Alpine;
+app.mount("#app");
 Alpine.start();
