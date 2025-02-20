cd8ce4be286d731bcb3191078aa8582b6ddae72d
GarzonMiguelA <miguel.garzon.7e7@itb.cat>
2025-02-20 15:59:56 +0100

    commit hardcoding of tables and applied styles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,24 @@
+<style>
+    /* A parte de trabajar con tailwind css */
+    .tabla {
+        border: 3px solid #4adba4;
+    }
+
+    .tabla th,
+    .tabla td {
+        border: 2px solid #4adba4;
+    }
+
+    .tabla thead {
+        background-color: #4adba4;
+        color: white;
+    }
+
+    .tabla tbody tr:hover {
+        background-color: rgba(74, 219, 164, 0.1);
+    }
+</style>
+
 <x-app-layout>
     <x-slot name="header">
         <link rel="stylesheet" href="build/css/styles.css">
@@ -10,7 +31,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                    <table class="tabla min-w-full divide-y">
                         <thead>
                             <tr>
                                 <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Nombre</th>
@@ -48,6 +69,28 @@
                                 <td class="px-6 py-4 whitespace-nowrap">39.4543</td>
                                 <td class="px-6 py-4 whitespace-nowrap">-0.3555</td>
                                 <td class="px-6 py-4 whitespace-nowrap">Complejo arquitectónico cultural y de entretenimiento</td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900 mr-3">Ver en mapa</a>
+                                    <button class="text-red-600 hover:text-red-900">Eliminar</button>
+                                    <button class="text-green-600 hover:text-red-900">Editar</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">Mercado Central</td>
+                                <td class="px-6 py-4 whitespace-nowrap">39.4736</td>
+                                <td class="px-6 py-4 whitespace-nowrap">-0.3783</td>
+                                <td class="px-6 py-4 whitespace-nowrap">Mercado histórico de Valencia</td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900 mr-3">Ver en mapa</a>
+                                    <button class="text-red-600 hover:text-red-900">Eliminar</button>
+                                    <button class="text-green-600 hover:text-red-900">Editar</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">Bioparc Valencia</td>
+                                <td class="px-6 py-4 whitespace-nowrap">39.4789</td>
+                                <td class="px-6 py-4 whitespace-nowrap">-0.4062</td>
+                                <td class="px-6 py-4 whitespace-nowrap">Parque zoológico de Valencia</td>
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <a href="#" class="text-indigo-600 hover:text-indigo-900 mr-3">Ver en mapa</a>
                                     <button class="text-red-600 hover:text-red-900">Eliminar</button>
