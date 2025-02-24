c968a225b40a7408a8c7fb9c87d661402e716c3a
TeoCastellvi <teo.castellvi.7e7@itb.cat>
2025-02-24 12:50:59 +0100

    progres bar acabada

--- resources/views/dadesClient.blade.php
+++ resources/views/dadesClient.blade.php
@@ -10,32 +10,36 @@
 </head>
 <body>
     <x-app-layout>
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Buscador de Direcció') }}
-            </h2>
-        </x-slot>
+        
         <!-- barra de progres -->
         <div class="progress-bar-container mt-5">
             <div class="progress-bar bg-white border flex justify-center items-center mx-auto shadow-teal-300 shadow-md max-w-6xl p-2 rounded-lg dark:bg-gray-700 dark:text-gray-300">
                 <div class="w-full max-w-screen-2xl px-12 mx-auto">
                     <ul class="w-full flex flex-col md:flex-row justify-center items-center gap-40 mt-2 md:mt-0 md:text-base md:font-medium">
-                        <li class="flex flex-col items-center">
-                            <div class="w-10 h-10 flex items-center justify-center bg-emerald-400 border-2 border-emerald-400 rounded-full text-white font-bold text-lg">1</div>
-                            <span class="text-gray-700 dark:text-white text-sm md:text-base">Dades del Client</span>
+                        <li class="flex flex-col items-center cursor-pointer transition-transform duration-200 ease-in-out hover:scale-110">
+                            <a href="dades" class="flex flex-col items-center">
+                                <div class="w-10 h-10 flex items-center justify-center bg-emerald-400 border-2 border-emerald-400 rounded-full text-white font-bold text-lg">1</div>
+                                <span class="text-gray-700 dark:text-white text-sm md:text-base mt-1">Dades del Client</span>
+                            </a>
                         </li>
-                        <li class="flex flex-col items-center">
-                            <div class="w-10 h-10 flex items-center justify-center bg-white border-2 border-emerald-400 rounded-full text-emerald-400 font-bold text-lg">2</div>
-                            <span class="text-gray-700 dark:text-white text-sm md:text-base">Seleccionar area</span>
+                        <li class="flex flex-col items-center cursor-pointer transition-transform duration-200 ease-in-out hover:scale-110">
+                            <a href="mapa" class="flex flex-col items-center">
+                                <div class="w-10 h-10 flex items-center justify-center bg-white border-2 border-emerald-400 rounded-full text-emerald-400 font-bold text-lg">2</div>
+                                <span class="text-gray-700 dark:text-white text-sm md:text-base mt-1">Seleccionar area</span>
+                            </a>
                         </li>
-                        <li class="flex flex-col items-center">
-                            <div class="w-10 h-10 flex items-center justify-center bg-white border-2 border-emerald-400 rounded-full text-emerald-400 font-bold text-lg">3</div>
-                            <span class="text-gray-700 dark:text-white text-sm md:text-base">Formulari Prova</span>
+                        <li class="flex flex-col items-center cursor-pointer transition-transform duration-200 ease-in-out hover:scale-110">
+                            <a href="formulari" class="flex flex-col items-center">
+                                <div class="w-10 h-10 flex items-center justify-center bg-white border-2 border-emerald-400 rounded-full text-emerald-400 font-bold text-lg">3</div>
+                                <span class="text-gray-700 dark:text-white text-sm md:text-base mt-1">Formulari Prova</span>
+                            </a>
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
+        
+        
         
         
         <div class="client-form-container">
