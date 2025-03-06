6a3b31c29a2814922b3544c5de17781ea71d4538
Thalia <thalia.soler.7e7@itb.cat>
2025-03-06 16:49:59 +0100

    Footer añadido correctamente

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,86 @@
+<footer id="main-footer" class="bg-[#34495E] text-white py-8">
+    <div class="container mx-auto px-4">
+        <div id="footer-widgets" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+            <div class="footer-widget">
+                <div id="text-10" class="fwidget et_pb_widget widget_text">
+                    <div class="textwidget">
+                        <p><img class="w-20 h-20 object-contain"
+                                src="https://www.lenium.es/wp-content/uploads/2019/08/logotipo-lenium-300x300.png"
+                                alt="Logotipo Lenium"></p>
+                        <p>Empresa líder en energías renovables. Más de 28 años de trabajo para mejorar la vida de nuestros clientes</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-widget">
+                <div id="text-7" class="fwidget et_pb_widget widget_text">
+                    <h4 class="font-bold text-lg mb-4 hover:text-[#49dba3]">PÁGINAS DE INTERÉS</h4>
+                    <div class="textwidget">
+                        <p><a href="https://www.lenium.es/en/lenium-2/" class="text-gray-300 hover:text-[#49dba3]">Lenium</a><br>
+                            <a href="https://www.lenium.es/renewable-energies/" class="text-gray-300 hover:text-[#49dba3]">Energias renovables</a><br>
+                            <a href="https://www.lenium.es/financing/" class="text-gray-300 hover:text-[#49dba3]">Financiación</a><br>
+                            <a href="https://www.lenium.es/rd/" class="text-gray-300 hover:text-[#49dba3]">R&D</a><br>
+                            <a href="https://www.lenium.es/en/projects/" class="text-gray-300 hover:text-[#49dba3]">Proyectos</a><br>
+                            <a href="https://www.lenium.es/blog-en/" class="text-gray-300 hover:text-[#49dba3]">Blog</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-widget">
+                <div id="text-5" class="fwidget et_pb_widget widget_text">
+                    <h4 class="font-bold text-lg mb-4 hover:text-[#49dba3]">CONTACTA CON NOSOTROS</h4>
+                    <div class="textwidget">
+                        <p>Av. de la Meridiana 308, Ent-G51, 08027, Barcelona (España)<br>
+                            (+34) 667 957 521<br>
+                            29, Rue Amr Ibn Ass, Nº26, Tangier (Morocco)<br>
+                            (+212) 668 168 449<br>
+                            <a href="mailto:info@lenium.es" class="text-gray-300 hover:text-[#49dba3]">info@lenium.es</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-widget">
+                <div id="lsi_widget-3" class="fwidget et_pb_widget widget_lsi_widget">
+                    <h4 class="font-bold text-lg mb-4 hover:text-[#49dba3]">REDES SOCIALES</h4>
+                    <ul class="flex space-x-4">
+                        <li>
+                            <a href="https://www.linkedin.com/company/lenium-group/" target="_blank"
+                                class="text-gray-300 hover:text-[#49dba3]" aria-label="LinkedIn">
+                                <i class="fab fa-linkedin"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/LeniumG" target="_blank" class="text-gray-300 hover:text-[#49dba3]"
+                                aria-label="Twitter">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <!--<h4 class="font-bold text-lg mt-8 mb-4 hover:text-[#49dba3]">LANGUAGES</h4>
+                    <ul class="flex space-x-4">
+                        <li><a href="https://www.lenium.es/" class="text-gray-300 hover:text-[#49dba3]">Français</a></li>
+                        <li><a href="https://www.lenium.es/en/home/" class="text-gray-300 hover:text-[#49dba3]">English</a></li>
+                    </ul> -->
+                </div>
+            </div>
+        </div>
+
+        <div id="et-footer-nav" class="mt-8">
+            <div class="container">
+                <ul class="flex space-x-4 justify-center">
+                    <li><a href="https://www.lenium.es/en/legal-warning/" class="text-gray-300 hover:text-[#49dba3]">Legal warning</a></li>
+                    <li><a href="https://www.lenium.es/en/privacy-police/" class="text-gray-300 hover:text-[#49dba3]">Privacy police</a></li>
+                    <li><a href="https://www.lenium.es/en/cookies-policy/" class="text-gray-300 hover:text-[#49dba3]">Cookies policy</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div id="footer-bottom" class="mt-8 border-t border-gray-600 pt-4">
+            <div class="container text-center">
+                <div id="footer-info">© 2025 Lenium | Todos los derechos reservados</div>
+            </div>
+        </div>
+    </div>
+</footer>
