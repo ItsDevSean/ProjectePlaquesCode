4bb355d3be71994d7c7941b619d9308cb8bd834f
marc.santolaya.7e7@itb.cat <marc.santolaya.7e7@itb.cat>
2025-03-12 10:49:18 +0100

    feat - creacion de modal.js para añadir el script del modal para eliminar proyectos.

--- resources/views/proyectos.blade.php
+++ resources/views/proyectos.blade.php
@@ -164,51 +164,9 @@
     >
         @csrf
         @method('DELETE')
-    </form>
-
-
-    <script>
-   
-        let projectNameToDelete = '';
-        let projectIdToDelete = '';
-
-        function openModal(nombreProyecto, idProyecto) {
-            projectNameToDelete = nombreProyecto;
-            projectIdToDelete = idProyecto;
-            document.getElementById('projectoName').textContent = nombreProyecto;
-            document.getElementById('deleteProjectForm').action = `{{ route('dades_clients.destroy', '') }}/${idProyecto}`;
-            document.getElementById('modal').classList.remove('hidden');
-        }
-
-        function closeModal() {
-            document.getElementById('modal').classList.add('hidden');
-        }
-
-        function confirmDeletion() {
-            const userInput = document.getElementById('confirmationInput').value;
-
-            if (userInput === projectNameToDelete) {
-                document.getElementById('deleteProjectForm').submit();
-            } else {
-                alert('El nombre del proyecto no coincide. Eliminación cancelada.');
-            }
-        }
-
-      
-        function openSidePanel(projectId) {
-
-            document.getElementById('overlay').classList.remove('hidden');
-            document.getElementById('sidePanel').classList.remove('hidden');
-        }
-
-        function closeSidePanel() {
-            
-            document.getElementById('overlay').classList.add('hidden');
-            document.getElementById('sidePanel').classList.add('hidden');
-        }
-    </script>
-
+    </form> 
     
     <script src="build/js/sidePanel.js"></script>
+    <script src="build/js/modal.js"></script>
     <script src="build/js/estado.js"></script>
 </x-app-layout>
