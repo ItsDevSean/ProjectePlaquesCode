f698ca0cb31ea1a9095047364e59d6e39e0fead3
TeoCastellvi <teo.castellvi.7e7@itb.cat>
2025-03-20 13:27:19 +0100

    IMplementacio eliminar obstacles individualment

--- resources/views/mapaPrueva.blade.php
+++ resources/views/mapaPrueva.blade.php
@@ -6,6 +6,7 @@
     <title>Buscador de Direcció</title>
     <link rel="stylesheet" href="build/css/styleMapa.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 
 <body>
