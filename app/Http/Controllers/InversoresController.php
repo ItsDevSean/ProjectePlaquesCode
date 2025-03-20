228ae2624b0ae816928b3d7563abbf9e7737350e
marcSantolayaSanchez <marc.santolaya.7e7@itb.cat>
2025-03-20 13:29:26 +0100

    feat - Arreglo de la view inversores y de su modal. Mucho mas notoria y presentable.

--- app/Http/Controllers/InversoresController.php
+++ app/Http/Controllers/InversoresController.php
@@ -43,4 +43,32 @@ class InversoresController extends Controller
 
         return redirect()->route('inversores.index')->with('success', 'Inversor creado correctamente.');
     }
+
+        public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nombre_inversor' => 'required|string|min:2|max:100',
+            'eficiencia' => 'required|numeric|between:0,999.99',
+            'tipo_instalacion' => 'required|string',
+            'garantia_material' => 'nullable|integer|min:0',
+            'potencia_nominal' => 'required|integer|min:1',
+            'descripcion' => 'nullable|string|min:0',
+            'fabricante_id' => 'required|integer|exists:fabricantes,id',
+            'microinversor' => 'required|boolean',
+            'garantia_fabricante' => 'required|integer|min:0',
+            'imagen_inversor' => 'nullable|url|max:2048',
+            'id_referencia' => 'nullable|string|max:50',
+        ]);
+
+        $inversor = Inversores::findOrFail($id);
+        $inversor->update($request->all());
+
+        return redirect()->route('inversores.index')->with('success', 'Inversor actualizado correctamente.');
+    }
+
+    public function edit($id)
+    {
+        $inversor = Inversores::findOrFail($id);
+        return response()->json($inversor);
+    }
 }
