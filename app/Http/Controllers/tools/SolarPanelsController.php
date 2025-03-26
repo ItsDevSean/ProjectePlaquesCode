03090500fb0204e345b69095795363386e79e469
SeanITB <sean.costelloe.7e5@itb.cat>
2025-03-26 17:41:52 +0100

    feat: implementing error

--- app/Http/Controllers/tools/SolarPanelsController.php
+++ app/Http/Controllers/tools/SolarPanelsController.php
@@ -56,14 +56,14 @@ class SolarPanelsController extends Controller
             'material_marco' => 'nullable|string',
             'color_panel' => 'nullable|string',
             'potencia_maxima' => 'required|numeric|min:0',
-            'tension_maxima_potencia' => 'required|numeric|min:0',
-            'corriente_punto_maxima_potencia' => 'required|numeric|min:0',
-            'tension_circuito_abierto' => 'required|numeric|min:0',
-            'corriente_cortocircuito' => 'required|numeric|min:0',
+            'tension_maxima_potencia' => 'nullable|numeric|min:0',
+            'corriente_punto_maxima_potencia' => 'nullable|numeric|min:0',
+            'tension_circuito_abierto' => 'nullable|numeric|min:0',
+            'corriente_cortocircuito' => 'nullable|numeric|min:0',
             'eficencia_panel' => 'required|numeric|min:0|max:100',
             'coeficiente_temp_pmax' => 'required|numeric|min:0|max:100',
-            'coeficiente_temp_voc' => 'required|numeric|min:0|max:100',
-            'coeficiente_temp_isc' => 'required|numeric|min:0|max:100',
+            'coeficiente_temp_voc' => 'nullable|numeric|min:0|max:100',
+            'coeficiente_temp_isc' => 'nullable|numeric|min:0|max:100',
         ]);
 
         SolarPanelsModel::create($request->all() + ['user_id' => Auth::id()]);
