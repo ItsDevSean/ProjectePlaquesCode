c59a2b65c45e3c2631f16664f8a5c4df63f5e48c
marcSantolayaSanchez <marc.santolaya.7e7@itb.cat>
2025-04-02 14:09:58 +0200

    feat - Mejora de la implementación de la view proyectos.blade.php. Ahora estan todas las rutas bien definidas y con completo funcionamiento. Arreglo de los colores. Los estados se actualizan automaticamente al cambiarlos y tienen sus respectivos colores. (Todo muy loco.)

---
+++ public/build/js/proyectos.js
@@ -0,0 +1,31 @@
+// Habilitar/deshabilitar botón de confirmación según coincidencia
+document.getElementById('confirmationInput').addEventListener('input', function() {
+    const projectName = document.getElementById('projectoName').textContent;
+    const confirmButton = document.getElementById('confirmDeleteButton');
+    confirmButton.disabled = this.value !== projectName;
+});
+
+function closeSidePanel() {
+    document.getElementById('overlay').classList.add('hidden');
+    document.getElementById('sidePanel').classList.remove('translate-x-0');
+    document.getElementById('sidePanel').classList.add('translate-x-full');
+}
+
+
+function openModal(projectName, projectId) {
+    document.getElementById('projectoName').textContent = projectName;
+    document.getElementById('deleteProjectForm').action = `/dades_clients/${projectId}`;
+    document.getElementById('modal').classList.remove('hidden');
+    document.getElementById('confirmationInput').value = '';
+    document.getElementById('confirmDeleteButton').disabled = true;
+}
+
+
+
+function closeModal() {
+    document.getElementById('modal').classList.add('hidden');
+}
+
+function confirmDeletion() {
+    document.getElementById('deleteProjectForm').submit();
+}
