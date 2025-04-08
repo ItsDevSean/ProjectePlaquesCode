eaa4160be8156a38c9fa6f2855042369d6bc5bca
marc.santolaya.7e7@itb.cat <marc.santolaya.7e7@itb.cat>
2025-04-08 15:49:25 +0200

    feat - Cambio en los colores de crear baterias/inversores

--- resources/views/baterias.blade.php
+++ resources/views/baterias.blade.php
@@ -23,7 +23,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <button id="openModal" class="px-4 py-2 bg-[#49DBA3] text-white rounded-lg shadow-lg hover:bg-[#36B89A] transition-all duration-300">
+                        <button id="openModal" class="px-4 py-2 bg-gradient-to-r from-emerald-500 to-teal-600 text-white rounded-lg shadow-lg hover:bg-[#36B89A] transition-all duration-300">
                             <i class="fas fa-plus"></i> Crear Bateria
                         </button>
                     </div>
