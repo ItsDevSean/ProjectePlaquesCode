4a789dbd9ac712492e85ea254517adb64a9e4282
SeanITB <sean.costelloe.7e5@itb.cat>
2025-04-22 19:18:17 +0200

    feat: checkout

--- resources/views/tools/panels.blade.php
+++ resources/views/tools/panels.blade.php
@@ -179,12 +179,12 @@
                                             </td>
                                             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                                 <div class="flex items-center justify-end space-x-3">
-                                                    <button onclick="openDetail({{ $p }}, {{ json_encode($nameAtributes) }})" class="text-blue-600 hover:text-blue-800 transition-colors p-1.5 rounded-full hover:bg-blue-50 dark:hover:bg-gray-700" title="Ver detalles">
+                                                    {{-- <button onclick="openDetail({{ $p }}, {{ json_encode($nameAtributes) }})" class="text-blue-600 hover:text-blue-800 transition-colors p-1.5 rounded-full hover:bg-blue-50 dark:hover:bg-gray-700" title="Ver detalles">
                                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                                         </svg>
-                                                    </button>
+                                                    </button> --}}
                                                     <button onclick="event.stopPropagation(); openEditModal({{ json_encode($p) }})" class="text-emerald-600 hover:text-teal-700 transition-colors p-1.5 rounded-full hover:bg-emerald-50 dark:hover:bg-gray-700" title="Editar">
                                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
