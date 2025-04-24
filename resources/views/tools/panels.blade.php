91a10f21bd10b84bb6b45a004e3bc43a58c6b190
SeanITB <sean.costelloe.7e5@itb.cat>
2025-04-24 10:51:06 +0200

    fix: seeing wich are the porblems with the import

--- resources/views/tools/panels.blade.php
+++ resources/views/tools/panels.blade.php
@@ -28,6 +28,7 @@
                                 @csrf
                                 <div class="flex items-center gap-4">
                                     <label class="flex flex-col items-center justify-center w-full border-2 border-gray-300 dark:border-gray-600 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 transition-colors duration-300 p-4">
+                                        {{-- toDo: 1r. comprobar q funciona 2n. js q amaga aques apartat i mostra el nom del fitxer --}}
                                         <div class="flex flex-col items-center justify-center">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-gray-400 mb-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
